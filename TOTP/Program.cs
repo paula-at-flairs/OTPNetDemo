c2a1b2b3fa1a6edb8ddd83847e9c844576d686f6
Paula Fahmy <paula.fahmy@flairstech.com>
2021-05-18 10:39:49 +0200

    Added OTP hashing for secure database saving.

--- TOTP/Program.cs
+++ TOTP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using OtpNet;
 using System;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -38,11 +39,15 @@ namespace TOTP
 
         public static Totp GenerateOTP(string secretString)
         {
-            int validForSeconds = 5;
+            int validForSeconds = 5 * 60;
             int otpLength = 4;
 
             byte[] secretBytes = Encoding.ASCII.GetBytes(secretString);
             var totp = new Totp(secretBytes, step: validForSeconds, totpSize: otpLength);
+
+            var hashed = totp.HashOTP();
+            var validHash = totp.ValidateOTPHash(hashed);
+
             return totp;
         }
 
@@ -51,6 +56,46 @@ namespace TOTP
             var totp = GenerateOTP(userId);
             bool isTokenValid = totp.VerifyTotp(otp, out _);
             return isTokenValid;
+        }
+    }
+
+    public static class TOTPExtensions
+    {
+        public static string HashOTP(this Totp totp)
+        {
+            var otp = totp.ComputeTotp();
+
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(otp, salt, 100000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+            
+            return Convert.ToBase64String(hashBytes);
+        }
+
+        public static bool ValidateOTPHash(this Totp totp, string hashedOTP)
+        {
+            /* Extract the bytes */
+            byte[] hashBytes = Convert.FromBase64String(hashedOTP);
+            
+            /* Get the salt */
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+            
+            /* Compute the hash on the password the user entered */
+            var pbkdf2 = new Rfc2898DeriveBytes(totp.ComputeTotp(), salt, 100000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            /* Compare the results */
+            for (int i = 0; i < 20; i++)
+                if (hashBytes[i + 16] != hash[i])
+                    return false;
+
+            return true;
         }
     }
 }
